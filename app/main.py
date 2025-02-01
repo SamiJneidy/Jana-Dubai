96360134032175c6940b2f8e940cac819ddbda45
SamiJneidy <samihanijneidy@gmail.com>
2025-02-01 18:37:51 +0300

    added server run in main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from .core.exceptions import ResourceNotFound, register_handlers
 from .routers import routers
+import uvicorn
 
 app = FastAPI()
 
@@ -23,3 +24,6 @@ register_handlers(app=app)
 @app.get(path="/", status_code=status.HTTP_200_OK, tags=["Health Check"])
 def health_check():
     return {"message": "Welcome to Jana Dubai, The server is running."}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
